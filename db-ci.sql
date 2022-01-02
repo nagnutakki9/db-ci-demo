3c0477d9785d0d32274653302330e3f4be53381c
nagnutakki9 <53839996+nagnutakki9@users.noreply.github.com>
2022-01-02 09:30:22 -0500

    Create db-ci.sql

---
+++ db-ci.sql
@@ -0,0 +1,6 @@
+DROP TABLE employees;
+CREATE TABLE employees (
+    first_name VARCHAR2(25) PRIMARY KEY,
+    last_name VARCHAR2(25)
+);
+INSERT INTO employees (first_name, last_name) VALUES('Nag', 'Nutakki');
