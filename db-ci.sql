3691dea488a2ee6812afe1489abe6f0dfd46d385
nagnutakki9 <53839996+nagnutakki9@users.noreply.github.com>
2022-01-02 09:52:15 -0500

    Update db-ci.sql

--- db-ci.sql
+++ db-ci.sql
@@ -1,6 +1,15 @@
-DROP TABLE employees;
+DROP TABLE departments CASCADE CONSTRAINTS;
+CREATE TABLE departments (
+    department_id NUMBER(3),
+    department_description VARCHAR2(50)
+);
+INSERT INTO departments (department_id, department_description) VALUES(1, 'Sales Department');
+INSERT INTO departments (department_id, department_description) VALUES(2, 'Marketing Department');
+
+DROP TABLE employees CASCADE CONSTRAINTS;
 CREATE TABLE employees (
     first_name VARCHAR2(25) PRIMARY KEY,
-    last_name VARCHAR2(25)
+    last_name VARCHAR2(25),
+    department_id number(3) FOREIGN KEY departments(department_id)
 );
-INSERT INTO employees (first_name, last_name) VALUES('Nag', 'Nutakki');
+INSERT INTO employees (first_name, last_name, department_id) VALUES('Nag', 'Nutakki', 1);
